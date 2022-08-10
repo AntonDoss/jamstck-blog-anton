d6ffd6a0daab653801728285a2dd5fe3e9e77303
Anton.Do <anton.do@ramotion.agency>
2022-08-10 19:51:26 +0300

    Add "Latest" component, basic styles, tlwnd config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,11 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import Latest from "../components/Latest";
 
-const IndexPage = ({ data: { articles } }) => {
-  return (
-    <main>
-      <h1>Hello world!</h1>
-      {articles.nodes.map((article) => {
-        return (
-          <div key={article.id}>
-            <p>{article.tag}</p>
-            <h2>{article.title}</h2>
-            <p>{article.date}</p>
-          </div>
-        );
-      })}
-    </main>
-  );
-};
-
-export default IndexPage;
+export default () => (
+  <main className="bg-lightGrey">
+    <h1 className="text-21md font-semibold">Newsroom</h1>
+    <Latest />
+  </main>
+);
 
 export const Head = () => <title>Home Page</title>;
-
-export const query = graphql`
-  {
-    articles: allDatoCmsArticle {
-      nodes {
-        title
-        id
-        date
-        tag
-        image {
-          url
-        }
-      }
-    }
-  }
-`;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGrey: "#f5f5f7",
+        labelGrey: "#6e6e73",
+      },
+      fontSize: {
+        "32xl": "32px",
+        "21md": "21px",
+        "28md": "28px",
+        "19sm": "19px",
+      },
+      // container: {
+      //   screens: {
+      //     sm: "345px",
+      //     md: "680px",
+      //     lg: "1148px",
+      //     xl: "1280px",
+      //     "2xl": "1536px",
+      //   },
+      // },
+    },
   },
   plugins: [],
 };
