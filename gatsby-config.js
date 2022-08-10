05b1f5e3bc40b7eca973aa2e2608febeb3d8e844
Anton.Do <anton.do@ramotion.agency>
2022-08-10 09:45:59 +0300

    Add Tailwind CSS and automatic class sorting

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-source-datocms",
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,18 @@ import * as React from "react";
 import { graphql } from "gatsby";
 
 const IndexPage = ({ data: { articles } }) => {
-  const post = articles.nodes[0];
-
   return (
     <main>
       <h1>Hello world!</h1>
-      <p>{post.tag}</p>
-      <p>{post.title}</p>
-      <p>{post.date}</p>
-      {/* <img src={post.image.url}></img> */}
+      {articles.nodes.map((article) => {
+        return (
+          <div key={article.id}>
+            <p>{article.tag}</p>
+            <h2>{article.title}</h2>
+            <p>{article.date}</p>
+          </div>
+        );
+      })}
     </main>
   );
 };
