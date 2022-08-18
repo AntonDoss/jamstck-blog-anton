07b5e70321f43b32fe2ab605b7e90788f0d33438
Anton.Do <anton.do@ramotion.agency>
2022-08-18 19:45:33 +0300

    Add pagination

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="mb-5 mt-4 p-4 text-xs">
+      <div className="container mx-auto">
+        <div className="text-labelGrey">
+          <div className="">
+            More ways to shop: <a href="/">Find an Apple Store</a> or{" "}
+            <a href="/">other retailer</a> near you. Or call 1-800-MY-APPLE.
+          </div>
+          <div className="">
+            <div className="my-1 ">
+              Copyright &copy; 2022 Apple Inc. All rights reserved.
+            </div>
+            <div className="text-[#424545]">
+              <ul className="flex gap-2">
+                <li className="border-r pr-3">
+                  <a href="/">Privacy Policy</a>
+                </li>
+                <li className="border-r pr-3">
+                  <a href="/">Terms of Use</a>
+                </li>
+                <li className="border-r pr-3">
+                  <a href="/">Sales and Refunds</a>
+                </li>
+                <li className="border-r pr-3">
+                  <a href="/">Legal</a>
+                </li>
+                <li className="pr-3">
+                  <a href="/">Site Map</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,79 @@
 import * as React from "react";
-import { graphql, StaticQuery, Link } from "gatsby";
+import { graphql, Link } from "gatsby";
+import { useEffect, useState } from "react";
 
 import Layout from "../components/Layout";
 
-export default () => (
-  <Layout>
-    <main className="bg-lightGrey">
-      <StaticQuery
-        query={graphql`
-          {
-            articles: allDatoCmsArticle(limit: 2) {
-              nodes {
-                slug
-                title
-                id
-                tag
-                image {
-                  url
-                }
-                topics {
-                  topic
-                  slug
-                  id
-                }
-                meta {
-                  publishedAt(formatString: "MMMM D, YYYY")
-                }
-              }
-            }
-          }
-        `}
-        render={(data) => (
-          <div className="mx-auto mb-6 flex max-w-[366px] flex-col">
-            <h2 className="text-2xl font-bold md:text-28md lg:text-32xl">
-              Latest News
-            </h2>
-            {data.articles.nodes.map(
-              (article: {
-                slug: string;
-                id: string;
-                image: { url: string };
-                tag: string;
-                title: string;
-                meta: { publishedAt: string };
-              }) => {
+export default function Index({ data: { articles, articlesPerPage } }: any) {
+  const articlesOnPage = articlesPerPage.entityPayload.attributes.number;
+  const [allArticles] = useState(articles.nodes);
+  const [pagination, setPagination] = useState(Array);
+  const [loaded, setLoaded] = useState(false);
+  const [limit] = useState(articlesOnPage);
+  const [pages] = useState(Math.ceil(allArticles.length / limit));
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const goToNextPage = () => {
+    setCurrentPage((page) => page + 1);
+  };
+
+  const goToPreviousPage = () => {
+    setCurrentPage((page) => page - 1);
+  };
+
+  const changePage = (event: any) => {
+    const pageNumber = Number(event.target.textContent);
+    setCurrentPage(pageNumber);
+  };
+
+  useEffect(() => {
+    let pagesNumber = [...Array(Math.ceil(articles.totalCount / limit))].map(
+      (_, i) => i + 1
+    );
+    console.log(articlesOnPage);
+
+    setPagination(pagesNumber);
+    setLoaded(true);
+  }, [allArticles, limit]);
+
+  const getPaginatedArticles = () => {
+    const startIndex = currentPage * limit - limit;
+    const endIndex = startIndex + limit;
+    return allArticles.slice(startIndex, endIndex);
+  };
+
+  const getPaginationGroup = () => {
+    let start = Math.floor((currentPage - 1) / limit) * limit;
+    let end = start + limit;
+
+    return pagination.slice(start, end + 1);
+  };
+
+  return (
+    <Layout>
+      <main className="bg-lightGrey">
+        <div className="mx-auto mb-6 max-w-[366px] md:max-w-[692px]">
+          <h2 className="mb-4 text-2xl font-bold md:text-28md lg:text-32xl">
+            Latest News
+          </h2>
+          <div className="flex flex-col">
+            {getPaginatedArticles().map(
+              (
+                article: {
+                  slug: string;
+                  id: string;
+                  image: { url: string };
+                  tag: string;
+                  title: string;
+                  meta: { publishedAt: string };
+                },
+                index: number
+              ) => {
                 return (
                   <Link to={`/posts/${article.slug}`} key={article.id}>
-                    <div className="mt-3.5 border-solid">
-                      <img
-                        className="rounded-t-xl"
-                        src={article.image.url}
-                        alt=""
-                      ></img>
-                      <div className="flex flex-col justify-between rounded-b-xl bg-white p-6">
+                    <div className="mt-4 flex flex-col overflow-hidden rounded-xl">
+                      <img className="" src={article.image.url} alt=""></img>
+                      <div className="flex flex-col justify-between bg-white p-6">
                         <p className="label mb-2 text-xs font-bold uppercase text-labelGrey">
                           {article.tag}
                         </p>
@@ -69,10 +90,77 @@ export default () => (
               }
             )}
           </div>
-        )}
-      />
-    </main>
-  </Layout>
-);
+          <div className="flex justify-between">
+            <button
+              onClick={goToPreviousPage}
+              className={currentPage === 1 ? "prev disabled" : "prev"}
+            >
+              Prev
+            </button>
+
+            <p>
+              {currentPage} of {pages}
+            </p>
+
+            {/* {loaded &&
+              getPaginationGroup().map((pageNumber: any, index) => {
+                return (
+                  <div key={index}>
+                    <button
+                      onClick={changePage}
+                      className={
+                        currentPage === pageNumber ? "active font-bold" : ""
+                      }
+                    >
+                      {pageNumber}
+                    </button>
+                  </div>
+                );
+              })} */}
+
+            <button
+              onClick={goToNextPage}
+              className={currentPage === pages ? "next disabled" : "next"}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query allDatoCmsArticles {
+    articles: allDatoCmsArticle {
+      nodes {
+        slug
+        title
+        id
+        tag
+        image {
+          url
+        }
+        topics {
+          topic
+          slug
+          id
+        }
+        meta {
+          publishedAt(formatString: "MMMM D, YYYY")
+        }
+      }
+      totalCount
+    }
+    articlesPerPage: datoCmsArticlesPerPage {
+      entityPayload {
+        attributes {
+          number
+        }
+      }
+    }
+  }
+`;
 
 export const Head = () => <title>Home Page</title>;
